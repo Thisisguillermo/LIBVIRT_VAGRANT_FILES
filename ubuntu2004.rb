242b1afa7592c292e51c1a9aa058a32d52f1e706
Guillermo <guillermozaandam@gmail.com>
2020-06-01 02:53:49 +0200

    config.vm.synced_folder "./", "/vagrant"

--- ubuntu2004.rb
+++ ubuntu2004.rb
@@ -7,5 +7,6 @@ Vagrant.configure("2") do |config|
     libvirt.cpus = 4
     libvirt.memory = 4000
     config.vm.network "private_network", ip: "192.168.122.246"
+    config.vm.synced_folder "./", "/vagrant"
    end
 end
