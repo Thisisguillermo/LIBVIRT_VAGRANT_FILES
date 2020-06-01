72c0d30b9e3fd2c5cebefef12265b59beced6df5
Guillermo <guillermozaandam@gmail.com>
2020-06-01 15:41:36 +0200

    ...

--- ubuntu1804.rb
+++ ubuntu1804.rb
@@ -5,5 +5,7 @@ Vagrant.configure("2") do |config|
   config.vm.provider :libvirt do |libvirt|
     libvirt.cpus = 8
     libvirt.memory = 6000
+    config.vm.network "private_network", ip: "192.168.122.246"
+    config.vm.synced_folder "./", "/vagran
    end
 end
