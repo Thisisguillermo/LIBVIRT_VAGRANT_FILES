3c1bb6e064e03b07b9c2ad9a09aa33dc59d6e723
Guillermo <guillermozaandam@gmail.com>
2020-06-01 02:40:57 +0200

    private_network

--- ubuntu2004.rb
+++ ubuntu2004.rb
@@ -5,7 +5,7 @@ Vagrant.configure("2") do |config|
   config.vm.box = "generic/ubuntu2004"
   config.vm.provider :libvirt do |libvirt|
     libvirt.cpus = 4
-    libvirt.cputopology :sockets => '2', :cores => '2', :threads => '1'
     libvirt.memory = 4000
+    config.vm.network "private_network", ip: "192.168.122.246"
    end
 end
