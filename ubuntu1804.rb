bb7ec7bb634aeef0a3318137395b280c115881a8
Guillermo <guillermozaandam@gmail.com>
2020-06-05 00:04:18 +0200

    ...

--- ubuntu1804.rb
+++ ubuntu1804.rb
@@ -1,11 +1,14 @@
-# Vagrantfile ubuntu1804
-
 Vagrant.configure("2") do |config|
-  config.vm.box = "generic/ubuntu1804"
+  config.vm.box = "generic/ubuntu2004"
   config.vm.provider :libvirt do |libvirt|
-    libvirt.cpus = 8
-    libvirt.memory = 6000
+    libvirt.cpus = 4
+    libvirt.memory = 4000
     config.vm.network "private_network", ip: "192.168.122.246"
-    config.vm.synced_folder "./", "/vagrant"
+    #config.vm.synced_folder "./", "/vagrant"
+    Vagrant.configure("2") do |config|
+         config.vm.provision "shell",
+        inline: "sudo apt-get update -y && sudo apt-get upgrade -y"
+end
    end
 end
+
